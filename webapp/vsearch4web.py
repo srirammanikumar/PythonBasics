5e44c095d2db746fdd3ef21631d843f70477902f
Sriram Pallapothu <sriram.manikumar@gmail.com>
2019-02-27 23:57:06 -0500

    escape method, using sep in print

--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, escape
 from vsearch import search4letters
 
 app = Flask(__name__)
@@ -24,18 +24,22 @@ def do_search() -> 'html':  # accept form's i/p, hence POST
 
 def log_request(req: 'flask_request', res: str) -> None:
     with open('vsearch.log', 'a') as log:
-        print(req, res, file=log)
+        print(req.form, file=log, end='|')
+        print(req.remote_addr, file=log, end='|')
+        print(req.user_agent, file=log, end='|')
+        print(res, file=log)
+        # asn alternate to above 4 lines - use sep
+        # print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
 
 
 @app.route('/viewlog')
 def view_the_log() -> str:
     with open('vsearch.log', 'r') as log:
-        contents = log.read() # .read() method will read the entire file
-    return contents
+        contents = log.read()  # .read() method will read the entire file
+    return escape(contents)
 
 
-
-# @app.route('/')    # you coulld also do this instead of redirect from hello(). this will be less number of requests.
+# @app.route('/')    # you could also do this instead of redirect from hello(). this will be less number of requests.
 @app.route('/entry')
 def entry_page() -> 'html':
     return render_template('entry.html', the_title='Welcome to search4letters web')
